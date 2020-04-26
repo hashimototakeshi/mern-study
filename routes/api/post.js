d3718b681f7104ed7b41522fb9b1b4fd0fe276e6
Hashimoto Takeshi <hashimoto@no1s.biz>
2020-04-26 16:36:20 +0900

    mv

---
+++ routes/api/post.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+/**
+ * @route GET api/users
+ * @desc  Register user
+ * @access Public
+ */
+router.get("/", (req, res) => {
+  res.send("Post route");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const connectDB = require("./config/db.js");
 
 const app = express();
+
+connectDB();
+//init middleware
+app.use(express.json({ extended: false }));
+
+app.use("/api/users", require("./routes/api/users.js"));
+app.use("/api/auth", require("./routes/api/auth.js"));
+app.use("/api/profile", require("./routes/api/profile.js"));
+app.use("/api/posts", require("./routes/api/post.js"));
+
 
 app.get("/", (req, res) => {
   res.send("API Running");
